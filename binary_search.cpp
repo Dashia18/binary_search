3829bee17e81564d3c336a055ffbfcc4a8987bcf
Daria Bobkova <das-bobkova@yandex.ru>
2015-04-29 23:16:02 +0300

    First result

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,12 +1,54 @@
 #include "binary_search.hpp"
+#include <math.h>
 
 int binary_search(const std::vector<int>& v, int val)
 {
-	for(int i = 0; i < v.size(); i++)
+	int val_exist = 0; //element exist or not in array
+	int val_position; // value position (чем-то определить надо все равно но чем...??)
+	double middle_position;
+	int vector_size = v.size();
+	
+																cout<<"Array:\n";
+																for(int i = 0; i < v.size(); i++)
+																{
+																	cout<<v[i]
+																		<<' ';
+																}
+																cout<<"\nvector_size: ";
+																cout<<vector_size;
+
+	int array_el_min = 0;
+	int array_el_max = vector_size-1;
+	
+	do
 	{
-		cout<<v[i]
-			<<' ';
+	middle_position = (array_el_max + array_el_min)/2; //??
+	//middle_position = ceil(middle_position); //??
+																cout<<"\nmiddle_position: ";
+																cout<<middle_position
+																	<<"\nelenent in middle position: "
+																	<<v[middle_position];
+	if (v[middle_position] > val)
+	{
+		//v[0 .. middle_position]
+		array_el_min = 0;
+		array_el_max = middle_position;
 	}
+	else if (v[middle_position] < val)
+	{
+		//v[middle_position .. end]
+		array_el_min = middle_position;
+		array_el_max = array_el_max;
+	} 
+	else
+	{
+		val_position = middle_position;
+		val_exist=1;
+		cout<< "\nElement was find! It position is:\n"
+	    <<val_position;
+	}
+	}while(val_exist!=1);
+
     return 0;
 }
 
--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 int main()
 {
-	const int N = 5, val = 4;	
-	vector<int> v(0);
+	const int N = 6, val = 4;	
+	vector<int> v;
 	for (int i = 0; i < N; i++)
 	{
-		v.push_back(i);
+		v.push_back(i+1);
 	}
 
 	binary_search(v, val);
